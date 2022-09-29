2d5d5bbb147cd74edb6fea5325bc91e32e1288b1
Konstantin Teslenko <kostya5779025@gmail.com>
2022-09-29 14:08:02 +0300

    Format code

--- inc/music.h
+++ inc/music.h
@@ -1,15 +1,13 @@
-#ifndef MUSIC_H
-#define MUSIC_H
+#pragma once
+
 #include <stdbool.h>
-#include <time.h>
 #include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-typedef struct{
-	Mix_Music* m_arr;
-}t_mus;
+
+typedef struct {
+    Mix_Music* m_arr;
+} t_mus;
+
 bool init_music(t_mus* m);
 void del_music(t_mus* m );
 Mix_Music* load_music(const char* str);
 void play_music(t_mus* m);
-#endif
--- src/music.c
+++ src/music.c
@@ -1,43 +1,39 @@
 #include "music.h"
-bool init_music(t_mus* mus){
-//Initialize SDL_mixer
-                if( Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 )
-                {
-			return false;
-			printf( "SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError() );
-                }
+
+bool init_music(t_mus *mus) {
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+		return false;
+		printf("SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
+	}
 	*mus = (t_mus){NULL};
-	mus->m_arr =(Mix_Music*)load_music("resources/music/spirit-blossom-15285.wav");
+	mus->m_arr = (Mix_Music *)load_music("resources/music/spirit-blossom-15285.wav");
 	return true;
 }
-Mix_Music* load_music(const char* str){
-		 //Load music
-	Mix_Music*    gMusic = Mix_LoadMUS(str);
-    if( gMusic == NULL )
-    {
-	    printf( "Failed to load beat music! SDL_mixer Error: %s\n", Mix_GetError() );
-            return NULL;
-    }
 
-        printf( "Not failed to load beat music! SDL_mixer Error: \n" );
-    return gMusic;
-}
-void play_music(t_mus* m){
-	if(m==NULL){
-
-	    printf( "Failed to play beat music! SDL_mixer Error: %s\n", Mix_GetError() );
-            return;
+Mix_Music *load_music(const char *str) {
+	Mix_Music *gMusic = Mix_LoadMUS(str);
+	if (gMusic == NULL) {
+		printf("Failed to load beat music! SDL_mixer Error: %s\n", Mix_GetError());
+		return NULL;
 	}
-	if( Mix_PlayingMusic() == 0 )
-                            {
-                             //Play the music
-                                Mix_PlayMusic(m->m_arr, -1 );
-			    }
+
+	printf("Not failed to load beat music! SDL_mixer Error: \n");
+	return gMusic;
 }
 
-void del_music(t_mus* mus){
+void play_music(t_mus *m) {
+	if (m == NULL) {
 
-		Mix_FreeMusic( (mus->m_arr) );
-    		(mus->m_arr) = NULL;
-    Mix_Quit();
+		printf("Failed to play beat music! SDL_mixer Error: %s\n", Mix_GetError());
+		return;
+	}
+	if (Mix_PlayingMusic() == 0) {
+		Mix_PlayMusic(m->m_arr, -1);
+	}
+}
+
+void del_music(t_mus *mus) {
+	Mix_FreeMusic((mus->m_arr));
+	(mus->m_arr) = NULL;
+	Mix_Quit();
 }
